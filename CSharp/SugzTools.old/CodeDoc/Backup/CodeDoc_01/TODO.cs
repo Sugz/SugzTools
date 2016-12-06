468348a52bd00de3bc46d407f127d847ca69e0ea
Sugz <plantec.clement@gmail.com>
2016-12-07 00:05:09 +0100

    Remove Backups and SugzTools.old

--- CSharp/SugzTools.old/CodeDoc/Backup/CodeDoc_01/TODO.cs
+++
@@ -1,4 +0,0 @@
-﻿// TODO: au lieu d'utiliser un StringCollection pour les treeview item, utiliser directement un flow document (ou un paragraph) qui sera appeler par le view model et injecter dans la view, et utiliser directement les descritpion parser dans les models
-// TODO: Clean les parsers
-// TODO: Modifier la description des fonctions: plutot que <Var> ou <Array <Var>>, mettre directement Var ou Array<Var> / Array<Type1, Type2, etc...> 
-// TODO: Décider que faire avec le Optionnal / Var et autre

d635f91f1e8e06ae30b81cb9cca71feb056c7b53
Sugz <plantec.clement@gmail.com>
2016-12-09 23:16:09 +0100

    Update SgzProgressBar
    Implement new properties to get a Cylon mode

--- CSharp/SugzTools/Controls/SgzProgressBar/SgzProgressBar.cs
+++ CSharp/SugzTools/Controls/SgzProgressBar/SgzProgressBar.cs
@@ -12,8 +12,19 @@ using System.Windows.Media.Animation;
 
 namespace SugzTools.Controls
 {
+    [TemplatePart(Name = "PART_Border", Type = typeof(Border))]
     public class SgzProgressBar : ProgressBar
     {
+
+        #region Fields
+
+
+        Border PART_Border;                             // The main border where is define the VisualStateGroup
+        //private bool _IndeterminateAutoReverse;         // 
+        //private bool _CylonMode;
+
+        #endregion Fields
+
 
 
         #region Properties
@@ -30,7 +41,6 @@ namespace SugzTools.Controls
         }
 
 
-
         /// <summary>
         /// Get or set the Indeterminate brush
         /// </summary>
@@ -41,35 +51,58 @@ namespace SugzTools.Controls
             set { SetValue(IndeterminateBrushProperty, value); }
         }
 
-        // DependencyProperty as the backing store for IndeterminateBrush
-        public static readonly DependencyProperty IndeterminateBrushProperty = DependencyProperty.Register(
-            "IndeterminateBrush",
-            typeof(Brush),
-            typeof(SgzProgressBar),
-            new PropertyMetadata(Resource<SolidColorBrush>.GetColor("MaxBlue"))
-        );
 
-
-        /*TODO: FIND A WAY TO IMPLEMENT :(:(:(:(
         /// <summary>
-        /// 
+        /// Get or set if the animation should be autoreversed when the control is in Indeterminate state 
         /// </summary>
-        [Description(""), Category("Appearance")]
+        [Description("Get or set if the animation should be autoreversed when the control is in Indeterminate state "), Category("Appearance")]
         public bool IndeterminateAutoReverse
         {
             get { return (bool)GetValue(IndeterminateAutoReverseProperty); }
             set { SetValue(IndeterminateAutoReverseProperty, value); }
         }
 
-        // DependencyProperty as the backing store for IndeterminateAutoReverse
-        public static readonly DependencyProperty IndeterminateAutoReverseProperty = DependencyProperty.Register(
-            "IndeterminateAutoReverse",
-            typeof(bool),
-            typeof(SgzProgressBar),
-            new PropertyMetadata(false)
-        );
-        */
+        //public bool IndeterminateAutoReverse
+        //{
+        //    get { return _IndeterminateAutoReverse; }
+        //    set
+        //    {
+        //        _IndeterminateAutoReverse = value;
+        //        if (IsIndeterminate && PART_Border != null)
+        //        {
+        //            if (value)
+        //                VisualStateManager.GoToElementState(PART_Border, "Cylon", true);
+        //            else
+        //                VisualStateManager.GoToElementState(PART_Border, "Indeterminate", true);
+        //        }
+        //    }
+        //}
 
+
+
+
+
+        /// <summary>
+        /// The brush use for the Cylon Mode
+        /// </summary>
+        [Description("The brush use for the Cylon Mode"), Category("Brush")]
+        public Brush CylonBrush
+        {
+            get { return (Brush)GetValue(CylonBrushProperty); }
+            set { SetValue(CylonBrushProperty, value); }
+        }
+
+
+
+        /// <summary>
+        /// Get or set the Cylon Mode. If true, switch to Indeterminate, IndeterminateBrush become red and IndeterminateAutoReverse is true, false switch everything back to normal
+        /// </summary>
+        [Description("Get or set the Cylon Mode. If true, switch to Indeterminate, IndeterminateBrush become red and IndeterminateAutoReverse is true, false switch everything back to normal"), Category("Appearance")]
+        public bool IsCylon
+        {
+            get { return (bool)GetValue(IsCylonProperty); }
+            set { SetValue(IsCylonProperty, value); }
+        }
 
 
 
@@ -90,6 +123,61 @@ namespace SugzTools.Controls
         );
 
 
+        // DependencyProperty as the backing store for IndeterminateBrush
+        public static readonly DependencyProperty IndeterminateBrushProperty = DependencyProperty.Register(
+            "IndeterminateBrush",
+            typeof(Brush),
+            typeof(SgzProgressBar),
+            new PropertyMetadata(Resource<SolidColorBrush>.GetColor("MaxBlue"))
+        );
+
+
+        // DependencyProperty as the backing store for CylonBrush
+        public static readonly DependencyProperty CylonBrushProperty = DependencyProperty.Register(
+            "CylonBrush",
+            typeof(Brush),
+            typeof(SgzProgressBar),
+            new PropertyMetadata(new SolidColorBrush(Colors.Red))
+        );
+
+
+        // DependencyProperty as the backing store for IsCylon
+        public static readonly DependencyProperty IsCylonProperty = DependencyProperty.Register(
+            "IsCylon",
+            typeof(bool),
+            typeof(SgzProgressBar),
+            new PropertyMetadata(false)//, OnIsCylonChanged)
+        );
+
+        private static void OnIsCylonChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            //SgzProgressBar control = d as SgzProgressBar;
+            //if ((bool)e.NewValue)
+            //    control.IndeterminateAutoReverse = true;
+        }
+
+
+        // DependencyProperty as the backing store for IndeterminateAutoReverse
+        public static readonly DependencyProperty IndeterminateAutoReverseProperty = DependencyProperty.Register(
+            "IndeterminateAutoReverse",
+            typeof(bool),
+            typeof(SgzProgressBar),
+            new PropertyMetadata(false, OnIndeterminateAutoReverseChanged)
+        );
+
+        private static void OnIndeterminateAutoReverseChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            SgzProgressBar control = d as SgzProgressBar;
+            if (control.IsIndeterminate && control.PART_Border != null)
+            {
+                if ((bool)e.NewValue)
+                    VisualStateManager.GoToElementState(control.PART_Border, "Cylon", true);
+                else
+                    VisualStateManager.GoToElementState(control.PART_Border, "Indeterminate", true);
+            }
+        }
+
+
         #endregion Dependency Properties
 
 
@@ -101,17 +189,40 @@ namespace SugzTools.Controls
         {
             DefaultStyleKeyProperty.OverrideMetadata(typeof(SgzProgressBar), new FrameworkPropertyMetadata(typeof(SgzProgressBar)));
         }
-
         public SgzProgressBar()
         {
-
-
+            
         }
 
 
         #endregion Constructors
 
 
+
+        #region Methods
+
+
+        #region Override
+
+
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+            Border border = GetTemplateChild("PART_Border") as Border;
+            if (border != null)
+            {
+                PART_Border = border;
+                //if (IsCylon)
+                //    IndeterminateAutoReverse = true;
+            }
+                
+        }
+
+
+        #endregion Override
+
+
+        #endregion Methods
     }
 
 }

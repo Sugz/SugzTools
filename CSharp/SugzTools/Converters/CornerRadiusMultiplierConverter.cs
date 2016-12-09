41bbd5bf6707914277846e16ad979f9b700e6e63
Sugz <plantec.clement@gmail.com>
2016-12-09 15:45:02 +0100

    Implement CornerRadiusMultiplierConverter

--- CSharp/SugzTools/Converters/CornerRadiusMultiplierConverter.cs
+++ CSharp/SugzTools/Converters/CornerRadiusMultiplierConverter.cs
@@ -5,7 +5,24 @@ using System.Windows.Data;
 
 namespace SugzTools.Converters
 {
-    class CornerRadiusMultiplierConverter
+    public class CornerRadiusMultiplierConverter : IValueConverter
     {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value != null && value is CornerRadius && parameter != null)
+            {
+                double topLeft = ((CornerRadius)value).TopLeft * System.Convert.ToDouble(parameter);
+                double topRight = ((CornerRadius)value).TopRight * System.Convert.ToDouble(parameter);
+                double bottomLeft = ((CornerRadius)value).BottomLeft * System.Convert.ToDouble(parameter);
+                double bottomRight = ((CornerRadius)value).BottomRight * System.Convert.ToDouble(parameter);
+                return new CornerRadius(topLeft, topRight, bottomRight, bottomLeft);
+            }
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
     }
 }

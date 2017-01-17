2b9fb05f7abc08168f9b5335aa121eff6d20dde4
Sugz <plantec.clement@gmail.com>
2017-01-17 01:10:23 +0100

    Update SvgToXaml\Folder.cs
    Remove Index property
    Add IEquatable and implement Equals() method

--- CSharp/SvgToXaml/Model/Folder.cs
+++ CSharp/SvgToXaml/Model/Folder.cs
@@ -1,19 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace SvgToXaml.Model
 {
-    public class Folder
+    public class Folder : IEquatable<Folder>
     {
         public string Name { get; set; }
-        public string Count { get; set; }
+        public int SvgCount { get; private set; }
+        
 
         public Folder(string name)
         {
             Name = name;
+            SvgCount = Directory.GetFiles(Name, "*.svg").Length;
+        }
+
+        public bool Equals(Folder other)
+        {
+            if (Name == other.Name && SvgCount == other.SvgCount)
+                return true;
+            return false;
         }
     }
 }

291a1ea955d77afc2bbc47d0def28f1990f2737d
Sugz <plantec.clement@gmail.com>
2017-01-19 23:43:21 +0100

    Add SugzTools\SgzExpanderListBox.cs

---
+++ CSharp/SugzTools/Controls/SgzExpanderListBox/SgzExpanderListBox.cs
@@ -0,0 +1,446 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Collections.Generic;
+
+namespace SugzTools.Controls
+{
+    /*
+    public class SgzExpanderItem : ListBoxItem
+    {
+
+        #region Properties
+
+        /// <summary>
+        /// Get or set the content for the Header
+        /// </summary>
+        public object Header { get; set; }
+
+        /// <summary>
+        /// Get or set the expand state
+        /// </summary>
+        public bool IsExpanded
+        {
+            get { return (bool)GetValue(IsExpandedProperty); }
+            set { SetValue(IsExpandedProperty, value); }
+        }
+
+
+        #endregion Properties
+
+
+        #region Dependency Properties
+
+
+        // DependencyProperty as the backing store for IsExpanded
+        public static readonly DependencyProperty IsExpandedProperty = DependencyProperty.Register(
+            "IsExpanded",
+            typeof(bool),
+            typeof(SgzExpanderItem),
+            new PropertyMetadata(true)
+        );
+
+
+        #endregion Dependency Properties
+
+
+        #region Constructors
+
+
+        public SgzExpanderItem()
+        {
+
+        }
+        public SgzExpanderItem(string header, bool isExpanded)
+        {
+            Header = header;
+            Content = header;
+            IsExpanded = isExpanded;
+        }
+
+
+        #endregion Constructors
+    }
+
+
+    public class SgzExpanderListBox : ListBox
+    {
+
+        #region Fields
+
+
+        private Point _DragStartPoint;
+        protected SgzExpanderItem _SourceItem;
+        protected SgzExpanderItem _TargetItem;
+
+
+        #endregion Fields
+
+
+        #region Constructor
+
+        static SgzExpanderListBox()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(SgzExpanderListBox), new FrameworkPropertyMetadata(typeof(SgzExpanderListBox)));
+        }
+        public SgzExpanderListBox()
+        {
+            PreviewMouseMove += List_PreviewMouseMove;
+            PreviewMouseLeftButtonDown += List_PreviewMouseLeftButtonDown;
+            Drop += List_Drop;
+        }
+
+
+        #endregion Constructor
+
+
+
+        #region Methods
+
+
+        #region Overrides
+
+
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+
+            // Add the DragEnter event handler to the ListBoxItem to catch _TargetItem 
+            Style itemContainerStyle = ItemContainerStyle;
+            if (itemContainerStyle != null)
+                itemContainerStyle.Setters.Add(new EventSetter(DragEnterEvent, new DragEventHandler(ListBoxItem_DragEnter)));
+
+        }
+
+
+        #endregion Overrides
+
+
+        #region Privates
+
+
+        private T FindVisualParent<T>(DependencyObject child)
+            where T : DependencyObject
+        {
+            do
+            {
+                if (child is T)
+                    return (T)child;
+
+                child = VisualTreeHelper.GetParent(child);
+            }
+            while (child != null);
+
+            return null;
+        }
+
+
+        #endregion Privates
+
+
+        #region Event Handlers
+
+
+        private void ListBoxItem_DragEnter(object sender, DragEventArgs e)
+        {
+            ListBoxItem lbi = FindVisualParent<ListBoxItem>(((DependencyObject)e.OriginalSource));
+            if (lbi != null && lbi != _SourceItem)
+                _TargetItem = lbi.DataContext as SgzExpanderItem;
+
+        }
+
+
+        private void List_PreviewMouseMove(object sender, MouseEventArgs e)
+        {
+            if (_SourceItem != null)
+            {
+                Point point = e.GetPosition(null);
+                Vector diff = _DragStartPoint - point;
+
+                if (e.LeftButton == MouseButtonState.Pressed &&
+                    (Math.Abs(diff.X) > SystemParameters.MinimumHorizontalDragDistance ||
+                        Math.Abs(diff.Y) > SystemParameters.MinimumVerticalDragDistance))
+                {
+                    SgzExpanderItem lbi = FindVisualParent<SgzExpanderItem>(((DependencyObject)e.OriginalSource));
+                    if (lbi != null)
+                        DragDrop.DoDragDrop(lbi, lbi.DataContext, DragDropEffects.Move);
+
+                }
+            }
+        }
+
+
+        private void List_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            _DragStartPoint = e.GetPosition(null);
+
+            SgzIcon dragIcon = FindVisualParent<SgzIcon>(((DependencyObject)e.OriginalSource));
+            if (dragIcon != null && dragIcon.Name == "PART_Drag")
+            {
+                SgzExpanderItem lbi = FindVisualParent<SgzExpanderItem>(((DependencyObject)e.OriginalSource));
+                if (lbi != null)
+                    _SourceItem = lbi.DataContext as SgzExpanderItem;
+
+            }
+        }
+
+
+        private void List_Drop(object sender, DragEventArgs e)
+        {
+            IList<SgzExpanderItem> items = DataContext as IList<SgzExpanderItem>;
+            if (items != null && _SourceItem != null)
+            {
+                int sourceIndex = Items.IndexOf(_SourceItem);
+                int targetIndex = Items.IndexOf(_TargetItem);
+
+                if (sourceIndex < targetIndex)
+                {
+                    items.Insert(targetIndex + 1, _SourceItem);
+                    items.RemoveAt(sourceIndex);
+                }
+                else
+                {
+                    int removeIndex = sourceIndex + 1;
+                    if (items.Count + 1 > removeIndex)
+                    {
+                        items.Insert(targetIndex, _SourceItem);
+                        items.RemoveAt(removeIndex);
+                    }
+                }
+
+                _SourceItem = null;
+            }
+        }
+
+
+        #endregion Event Handlers
+
+
+        #endregion Methods
+
+    }
+    */
+
+    
+    public class DragAndDropListBox<T> : ListBox
+        where T : class
+    {
+
+        #region Fields
+
+
+        private Point _dragStartPoint;
+        protected T _SourceItem;
+        protected T _TargetItem;
+
+
+        #endregion Fields
+
+
+
+        #region Constructor
+
+
+        public DragAndDropListBox()
+        {
+            AddEventHandlers();
+            SetItemContainerStyle();
+        }
+
+
+        #endregion Constructor
+
+
+
+        #region Methods
+
+
+        #region Privates
+
+
+        /// <summary>
+        /// Helper method to get the first VisualParent of a given type
+        /// </summary>
+        /// <typeparam name="P"></typeparam>
+        /// <param name="child"></param>
+        /// <returns></returns>
+        private P FindVisualParent<P>(DependencyObject child)
+            where P : DependencyObject
+        {
+            do
+            {
+                if (child is P)
+                    return (P)child;
+
+                child = VisualTreeHelper.GetParent(child);
+            }
+            while (child != null);
+            return null;
+        }
+
+
+        /// <summary>
+        /// Add various Event Handlers to handle drag and drop
+        /// </summary>
+        private void AddEventHandlers()
+        {
+            PreviewMouseMove += List_PreviewMouseMove;
+            PreviewMouseLeftButtonDown += List_PreviewMouseLeftButtonDown;
+            Drop += List_Drop;
+        }
+
+
+        /// <summary>
+        /// Set the ItemContainerStyle
+        /// </summary>
+        private void SetItemContainerStyle()
+        {
+            ControlTemplate template = new ControlTemplate(typeof(ListBoxItem));
+            template.VisualTree = new FrameworkElementFactory(typeof(ContentPresenter));
+
+            Style style = new Style(typeof(ListBoxItem));
+            style.Setters.Add(new Setter(TemplateProperty, template));
+            style.Setters.Add(new Setter(MarginProperty, new Thickness(0, 0, 0, 5)));
+            style.Setters.Add(new EventSetter(DragEnterEvent, new DragEventHandler(ListBoxItem_DragEnter)));
+
+            ItemContainerStyle = style;
+        }
+
+
+        #endregion Privates
+
+
+        #region Event Handlers
+
+
+        private void ListBoxItem_DragEnter(object sender, DragEventArgs e)
+        {
+            ListBoxItem lbi = FindVisualParent<ListBoxItem>(((DependencyObject)e.OriginalSource));
+            if (lbi != null && lbi != _SourceItem)
+                _TargetItem = lbi.DataContext as T;
+
+        }
+
+
+        private void List_PreviewMouseMove(object sender, MouseEventArgs e)
+        {
+            if (_SourceItem != null)
+            {
+                Point point = e.GetPosition(null);
+                Vector diff = _dragStartPoint - point;
+
+                if (e.LeftButton == MouseButtonState.Pressed &&
+                    (Math.Abs(diff.X) > SystemParameters.MinimumHorizontalDragDistance ||
+                        Math.Abs(diff.Y) > SystemParameters.MinimumVerticalDragDistance))
+                {
+                    ListBoxItem lbi = FindVisualParent<ListBoxItem>(((DependencyObject)e.OriginalSource));
+                    if (lbi != null)
+                        DragDrop.DoDragDrop(lbi, lbi.DataContext, DragDropEffects.Move);
+
+                }
+            }
+        }
+
+
+        private void List_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            _dragStartPoint = e.GetPosition(null);
+
+            SgzIcon dragIcon = FindVisualParent<SgzIcon>(((DependencyObject)e.OriginalSource));
+            if (dragIcon != null && dragIcon.Name == "PART_Drag")
+            {
+                ListBoxItem lbi = FindVisualParent<ListBoxItem>(((DependencyObject)e.OriginalSource));
+                if (lbi != null)
+                    _SourceItem = lbi.DataContext as T;
+
+            }
+        }
+
+
+        private void List_Drop(object sender, DragEventArgs e)
+        {
+            IList<T> items = DataContext as IList<T>;
+            if (items != null && _SourceItem != null)
+            {
+                int sourceIndex = Items.IndexOf(_SourceItem);
+                int targetIndex = Items.IndexOf(_TargetItem);
+
+                if (sourceIndex < targetIndex)
+                {
+                    items.Insert(targetIndex + 1, _SourceItem);
+                    items.RemoveAt(sourceIndex);
+                }
+                else
+                {
+                    int removeIndex = sourceIndex + 1;
+                    if (items.Count + 1 > removeIndex)
+                    {
+                        items.Insert(targetIndex, _SourceItem);
+                        items.RemoveAt(removeIndex);
+                    }
+                }
+
+                _SourceItem = null;
+            }
+        }
+
+
+        #endregion Event Handlers
+
+
+        #endregion Methods
+
+    }
+
+
+
+    public class SgzExpanderListBox : DragAndDropListBox<ExpanderItem>
+    {
+        static SgzExpanderListBox()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(SgzExpanderListBox), new FrameworkPropertyMetadata(typeof(SgzExpanderListBox)));
+        }
+    }
+    
+
+
+
+
+    public class ExpanderItem
+    {
+
+        #region Properties
+
+
+        public object Header { get; set; }
+        public bool IsExpanded { get; set; }
+        public object Content { get; set; }
+
+
+        #endregion Properties
+
+
+
+        #region Constructors
+
+
+
+        public ExpanderItem()
+        {
+
+        }
+        public ExpanderItem(string header, bool isExpanded)
+        {
+            Header = header;
+            Content = header;
+            IsExpanded = isExpanded;
+        }
+
+
+        #endregion Constructors
+
+    }
+    
+}
--- CSharp/TestApp/TestAppWindow.xaml.cs
+++ CSharp/TestApp/TestAppWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using SugzTools.Controls;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -21,40 +22,58 @@ namespace TestApp
     /// </summary>
     public partial class MainWindow : Window
     {
-
-
+        IList<ExpanderItem> _items;
 
         public MainWindow()
         {
             InitializeComponent();
-            intTxt2.Text = string.Format("Value: {0}, Type: {1}", intSpinner.Value, intSpinner.Value.GetType());
-            intTxt3.Text = string.Format("Scale: {0}", intSpinner.Scale);
-            floatTxt2.Text = string.Format("Value: {0}, Type: {1}", floatSpinner.Value, floatSpinner.Value.GetType());
-            floatTxt3.Text = string.Format("Scale: {0}", floatSpinner.Scale);
 
-            intSpinner.ValueChanged += (s, e) =>
+            _items = new ObservableCollection<ExpanderItem>()
             {
-                intTxt.Text = string.Format("Old: {0}, Type:{1}, New: {2}, Type:{3}", e.OldValue, e.OldValue.GetType(), e.NewValue, e.NewValue.GetType());
-                intTxt2.Text = string.Format("Value: {0}, Type: {1}", ((SgzNumericUpDown)s).Value, ((SgzNumericUpDown)s).Value.GetType());
-                intTxt3.Text = string.Format("Scale: {0}", ((SgzNumericUpDown)s).Scale);
-            };
-            floatSpinner.ValueChanged += (s, e) =>
-            {
-                floatTxt.Text = string.Format("Old: {0}, Type:{1}, New: {2}, Type:{3}", e.OldValue, e.OldValue.GetType(), e.NewValue, e.NewValue.GetType());
-                floatTxt2.Text = string.Format("Value: {0}, Type: {1}", ((SgzNumericUpDown)s).Value, ((SgzNumericUpDown)s).Value.GetType());
-                floatTxt3.Text = string.Format("Scale: {0}", ((SgzNumericUpDown)s).Scale);
+                new ExpanderItem("1", true),
+                new ExpanderItem("2", false),
+                new ExpanderItem("3", false),
+                new ExpanderItem("4", true),
+                new ExpanderItem("5", false),
+                new ExpanderItem("6", false),
             };
 
-            txt.Validate += (s, e) => Console.WriteLine(e.Text);
-            
+            Listbox.DataContext = _items;
+
         }
 
 
-        private void SgzButton_Click(object sender, RoutedEventArgs e)
-        {
-            progressbar.IndeterminateAutoReverse = !progressbar.IndeterminateAutoReverse; //NO
-            //progressbar.IsCylon = !progressbar.IsCylon;
-        }
+        //public MainWindow()
+        //{
+        //    InitializeComponent();
+        //    intTxt2.Text = string.Format("Value: {0}, Type: {1}", intSpinner.Value, intSpinner.Value.GetType());
+        //    intTxt3.Text = string.Format("Scale: {0}", intSpinner.Scale);
+        //    floatTxt2.Text = string.Format("Value: {0}, Type: {1}", floatSpinner.Value, floatSpinner.Value.GetType());
+        //    floatTxt3.Text = string.Format("Scale: {0}", floatSpinner.Scale);
+
+        //    intSpinner.ValueChanged += (s, e) =>
+        //    {
+        //        intTxt.Text = string.Format("Old: {0}, Type:{1}, New: {2}, Type:{3}", e.OldValue, e.OldValue.GetType(), e.NewValue, e.NewValue.GetType());
+        //        intTxt2.Text = string.Format("Value: {0}, Type: {1}", ((SgzNumericUpDown)s).Value, ((SgzNumericUpDown)s).Value.GetType());
+        //        intTxt3.Text = string.Format("Scale: {0}", ((SgzNumericUpDown)s).Scale);
+        //    };
+        //    floatSpinner.ValueChanged += (s, e) =>
+        //    {
+        //        floatTxt.Text = string.Format("Old: {0}, Type:{1}, New: {2}, Type:{3}", e.OldValue, e.OldValue.GetType(), e.NewValue, e.NewValue.GetType());
+        //        floatTxt2.Text = string.Format("Value: {0}, Type: {1}", ((SgzNumericUpDown)s).Value, ((SgzNumericUpDown)s).Value.GetType());
+        //        floatTxt3.Text = string.Format("Scale: {0}", ((SgzNumericUpDown)s).Scale);
+        //    };
+
+        //    txt.Validate += (s, e) => Console.WriteLine(e.Text);
+
+        //}
+
+
+        //private void SgzButton_Click(object sender, RoutedEventArgs e)
+        //{
+        //    progressbar.IndeterminateAutoReverse = !progressbar.IndeterminateAutoReverse; //NO
+        //    //progressbar.IsCylon = !progressbar.IsCylon;
+        //}
 
     }
 }

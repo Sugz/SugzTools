949f26d268468d62754aebedf434273e6c2cd1cc
Sugz <plantec.clement@gmail.com>
2017-01-22 23:47:50 +0100

    Add SugzTools\ExtensionMethods.cs
    Add ForEach() to ItemCollection as an extension method

---
+++ CSharp/SugzTools/Sources/ExtensionMethods.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace SugzTools.Src
+{
+    public static class ExtensionMethods
+    {
+
+        /// <summary>
+        /// Add ForEach method for ItemCollection
+        /// </summary>
+        /// <param name="items"></param>
+        /// <param name="action"></param>
+        public static void ForEach(this ItemCollection items, Action<object> action)
+        {
+            foreach (object item in items)
+                action(item);
+        }
+
+
+    }
+}

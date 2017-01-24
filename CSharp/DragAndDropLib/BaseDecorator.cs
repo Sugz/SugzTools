5bd117b5f928b638337bc2b9a17a09659288e128
Sugz <plantec.clement@gmail.com>
2017-01-25 00:48:33 +0100

    Add an Drag drop exemple

---
+++ CSharp/DragAndDropLib/BaseDecorator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Controls;
+using System.Windows;
+
+namespace DragAndDropLib
+{
+    public abstract class BaseDecorator : Decorator
+    {
+        protected UIElement DecoratedUIElement
+        {
+            get
+            {
+                if (this.Child is BaseDecorator)
+                {
+                    return ((BaseDecorator)this.Child).DecoratedUIElement;
+                }
+                return this.Child;
+            }
+        } 
+    }
+}

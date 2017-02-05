cec94e06d33671d6bcae9c943e3d88027ef3f499
Sugz <plantec.clement@gmail.com>
2017-02-05 21:17:11 +0100

    Udpate SugzTools\StyleApp
    Add two exemple for Attached and Blend behavior

---
+++ CSharp/StyleApp/Behaviors/PanelInnerMarginAttachedBehavior.cs
@@ -0,0 +1,49 @@
+﻿
+using SugzTools.Src;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace StyleApp
+{
+    public static class PanelInnerMarginAttachedBehavior
+    {
+        public static Thickness GetInnerMargin(DependencyObject obj)
+        {
+            return (Thickness)obj.GetValue(InnerMargin);
+        }
+        public static void SetInnerMargin(DependencyObject obj, Thickness value)
+        {
+            obj.SetValue(InnerMargin, value);
+        }
+        public static readonly DependencyProperty InnerMargin = DependencyProperty.RegisterAttached(
+            "InnerMargin", 
+            typeof(Thickness), 
+            typeof(PanelInnerMarginAttachedBehavior), 
+            new PropertyMetadata(new Thickness(0), (d, e) => ((Panel)d).Loaded += SetChildrenMargin)
+        );
+
+
+        private static void SetChildrenMargin(object sender, RoutedEventArgs e)
+        {
+            Panel panel = sender as Panel;
+            foreach (FrameworkElement child in panel.Children)
+            {
+                if (child == null)
+                    continue;
+
+                child.Margin = new Thickness(child.Margin.Left + GetInnerMargin(panel).Left,
+                    child.Margin.Top + GetInnerMargin(panel).Top,
+                    child.Margin.Right + GetInnerMargin(panel).Right,
+                    child.Margin.Bottom + GetInnerMargin(panel).Bottom
+                );
+            }
+
+            panel.Loaded -= SetChildrenMargin;
+        }
+    }
+}
---
+++ CSharp/StyleApp/Behaviors/PanelInnerMarginBehavior.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Interactivity;
+
+namespace StyleApp
+{
+    public class PanelInnerMarginBehavior : Behavior<Panel>
+    {
+
+        public Thickness InnerMargin { get; set; } = new Thickness(0);
+
+
+        protected override void OnAttached()
+        {
+            AssociatedObject.Loaded += SetChildrenMargin;
+            base.OnAttached();
+        }
+        
+
+        protected override void OnDetaching()
+        {
+            AssociatedObject.Loaded -= SetChildrenMargin;
+            base.OnDetaching();
+        }
+
+
+        private void SetChildrenMargin(object sender, RoutedEventArgs e)
+        {
+            Panel panel = sender as Panel;
+            foreach (FrameworkElement child in panel.Children)
+            {
+                if (child == null)
+                    continue;
+
+                child.Margin = new Thickness(child.Margin.Left + InnerMargin.Left,
+                    child.Margin.Top + InnerMargin.Top,
+                    child.Margin.Right + InnerMargin.Right,
+                    child.Margin.Bottom + InnerMargin.Bottom
+                );
+            }
+
+            panel.Loaded -= SetChildrenMargin;
+        }
+
+
+
+    }
+}

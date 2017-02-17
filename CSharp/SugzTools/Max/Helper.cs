fe0a069ed1c09441db397752204d33a1882b61ba
Sugz <plantec.clement@gmail.com>
2017-02-17 16:13:07 +0100

    Add Helper class to use inside max

---
+++ CSharp/SugzTools/Max/Helper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SugzTools.Max
+{
+    public static class Helper
+    {
+        /// <summary>
+        /// Check if an object has a method
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <param name="method"></param>
+        /// <returns></returns>
+        public static bool HasMethod(object obj, string method)
+        {
+            try { return obj.GetType().GetMethod(method) != null; }
+            catch (AmbiguousMatchException) { return true; }
+        }
+    }
+}

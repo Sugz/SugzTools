1a00619015600a8cd638a6f53554e7bcd5c960b2
Clément Plantec <plantec.clement@gmail.com>
2017-03-14 16:57:47 +0100

    update Sugztools

---
+++ CSharp/ConsoleApp1/Program.cs
@@ -0,0 +1,36 @@
+﻿using SugzTools.Src;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rename renamer = new Rename();
+            renamer.Name = "Test";
+
+            Console.WriteLine(renamer.Name);
+
+            renamer.AddPrefix("The-");
+            renamer.AddSuffix("-LH-");
+            Console.WriteLine(renamer.Name);
+
+            //renamer.RemoveLast(3);
+            //renamer.RemoveFirst(2);
+            //renamer.Remove(2, 2);
+
+            //renamer.Replace("-", "_", true);
+            //renamer.Replace("-", "_", false);
+            renamer.ReplaceAt(2, 2, "Add");
+
+            Console.WriteLine(renamer.Name);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ CSharp/SugzTools/Sources/Rename.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace SugzTools.Src
+{
+    public class Rename
+    {
+
+        public string Name { get; set; } = "";
+        public int Index { get; set; } = 1;
+        public int Padding { get; set; } = 2;
+
+
+        public void AddPrefix(string prefix) { Name = prefix + Name; }
+        public void AddSuffix(string suffix) { Name += suffix; }
+
+
+        public void Remove(int index, int length) { Name = Name.Remove(index, length); }
+        public void RemoveFirst(int length) { Name = Name.Substring(length); }
+        public void RemoveLast(int length) { Name = Name.Remove(Name.Length - length); }
+
+
+        public void Replace(string oldSubString, string newSubString, bool allOccurrences)
+        {
+            if (allOccurrences)
+            {
+                Name = Name.Replace(oldSubString, newSubString);
+            }
+            else
+            {
+                Regex regex = new Regex(Regex.Escape(oldSubString));
+                Name = regex.Replace(Name, newSubString, 1);
+            }
+        }
+        public void ReplaceAt(int index, int length, string newSubString)
+        {
+            Remove(index, length);
+            Name = Name.Insert(index, newSubString);
+        }
+
+
+        public void AddIndex(int increment)
+        {
+            string index = (Index + increment).ToString();
+            while (index.Length < Padding)
+                index = "0" + index;
+
+            Name += index;
+        }
+
+
+    }
+}

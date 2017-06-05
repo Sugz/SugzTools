0672499248215aa099492415e57d910620107ab8
Sugz <plantec.clement@gmail.com>
2017-06-05 23:33:21 +0200

    update sugztools

---
+++ CSharp/SugzTools/Controls/SgzTreeView/SgzTreeView.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace SugzTools.Controls
+{
+    public class SgzTreeView : TreeView
+    {
+
+        #region Fields
+
+
+
+
+
+        #endregion Fields
+
+
+        #region Properties
+
+
+
+
+
+        #endregion Properties
+
+
+        #region Dependency Properties
+
+
+
+
+
+        #endregion Dependency Properties
+
+
+        #region Constructors
+
+
+        static SgzTreeView()
+        {
+            //DefaultStyleKeyProperty.OverrideMetadata(typeof(SgzTreeView), new FrameworkPropertyMetadata(typeof(SgzTreeView)));
+        }
+        public SgzTreeView()
+        {
+
+        }
+
+
+        #endregion Constructors
+
+
+        #region Methods
+
+
+
+
+
+        #endregion Methods
+
+    }
+}

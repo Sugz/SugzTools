f8e6829ef60ec804c6b46076612a2ccf8d90f33e
Sugz <plantec.clement@gmail.com>
2017-08-29 01:45:03 +0200

    Update SugzTools

---
+++ CSharp/SugzTools/Controls/SgzCheckButton/SgzFlatCheckButton.cs
@@ -0,0 +1,108 @@
+﻿using SugzTools.Src;
+using SugzTools.Themes;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Media;
+
+namespace SugzTools.Controls
+{
+    public class SgzFlatCheckButton : ToggleButton
+    {
+
+        #region Properties
+
+
+        /// <summary>
+        /// Define the Checked brush
+        /// </summary>
+        [Description("Define the Checked brush"), Category("Brush")]
+        public Brush CheckedBrush
+        {
+            get { return (Brush)GetValue(CheckedBrushProperty); }
+            set { SetValue(CheckedBrushProperty, value); }
+        }
+
+
+        /// <summary>
+        /// Define the CheckMark brush
+        /// </summary>
+        [Description("Define the Checked brush"), Category("Brush")]
+        public Brush CheckMarkBrush
+        {
+            get { return (Brush)GetValue(CheckMarkBrushProperty); }
+            set { SetValue(CheckMarkBrushProperty, value); }
+        }
+
+
+        /// <summary>
+        /// Get or set the docking side of the check part
+        /// </summary>
+        [Description("Define the docking side of the check part."), Category("Appearance")]
+        public Dock PartCheckDock
+        {
+            get { return (Dock)GetValue(PartCheckDockProperty); }
+            set { SetValue(PartCheckDockProperty, value); }
+        }
+
+
+        #endregion
+
+
+
+        #region Dependency Properties
+
+
+        // DependencyProperty as the backing store for CheckedBrush
+        public static readonly DependencyProperty CheckedBrushProperty = DependencyProperty.Register(
+            "CheckedBrush",
+            typeof(Brush),
+            typeof(SgzFlatCheckButton),
+            new PropertyMetadata(Resource<SolidColorBrush>.GetColor("MaxBlue"))
+        );
+
+
+        // DependencyProperty as the backing store for CheckedBrush
+        public static readonly DependencyProperty CheckMarkBrushProperty = DependencyProperty.Register(
+            "CheckMarkBrush",
+            typeof(Brush),
+            typeof(SgzFlatCheckButton),
+            new PropertyMetadata(Resource<SolidColorBrush>.GetColor("MaxBlue"))
+        );
+
+
+        // DependencyProperty as the backing store for PartCheckDock
+        public static readonly DependencyProperty PartCheckDockProperty = DependencyProperty.Register(
+            "PartCheckDock",
+            typeof(Dock),
+            typeof(SgzFlatCheckButton),
+            new PropertyMetadata(Dock.Left)
+        );
+
+
+        #endregion Dependency Properties
+
+
+
+        #region Constructors
+
+
+        static SgzFlatCheckButton()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(SgzFlatCheckButton), new FrameworkPropertyMetadata(typeof(SgzFlatCheckButton)));
+        }
+        public SgzFlatCheckButton()
+        {
+            Loaded += (s, e) => FocusVisualStyle = FocusVisualStyles.GetControlStyle(3);
+        }
+
+
+        #endregion Constructors
+    }
+}
---
+++ CSharp/SugzTools/Converters/DockToAlignmentConverter.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Shapes;
+
+namespace SugzTools.Converters
+{
+    public class DockToAlignmentConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (values[0] is Dock dock && values[1] is Rectangle rectangle)
+            {
+                switch (dock)
+                {
+                    case Dock.Left:
+                        rectangle.HorizontalAlignment = HorizontalAlignment.Left;
+                        rectangle.VerticalAlignment = VerticalAlignment.Stretch;
+                        rectangle.Width = 2;
+                        break;
+                    case Dock.Top:
+                        rectangle.HorizontalAlignment = HorizontalAlignment.Stretch;
+                        rectangle.VerticalAlignment = VerticalAlignment.Top;
+                        rectangle.Height = 2;
+                        break;
+                    case Dock.Right:
+                        rectangle.HorizontalAlignment = HorizontalAlignment.Right;
+                        rectangle.VerticalAlignment = VerticalAlignment.Stretch;
+                        rectangle.Width = 2;
+                        break;
+                    case Dock.Bottom:
+                        rectangle.HorizontalAlignment = HorizontalAlignment.Stretch;
+                        rectangle.VerticalAlignment = VerticalAlignment.Bottom;
+                        rectangle.Height = 2;
+                        break;
+                    default:
+                        break;
+                }
+            }
+            return null;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

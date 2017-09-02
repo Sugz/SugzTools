683ca26fdf49f563fe570ae5d39ac0df1501de7c
Sugz <plantec.clement@gmail.com>
2017-09-02 16:26:19 +0200

    Update SugzTools

--- CSharp/ConsoleApp1/Program.cs
+++ CSharp/ConsoleApp1/Program.cs
@@ -20,7 +20,23 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            MaxscriptSynthaxCreator.Create();
+            string s = "activeGrid ambientColor ambientColorController animationRange animButtonEnabled animButtonState backgroundColor backgroundColorController backgroundImageFileName currentMaterialLibrary currentTime displayGamma fileInGamma fileOutGamma displaySafeFrames editorFont editorFontSize editorShowPath editorTabWidth escapeEnable environmentMap flyOffTime frameRate globalTracks hardwareLockID heapFree heapSize hotspotAngleSeparation inputTextColor lightLevel lightLevelController lightTintColor lightTintColorController listener localTime macroRecorder manipulateMode maxFileName maxFilePath messageTextColor numEffects numAtmospherics numSubObjectLevels outputTextColor playActiveOnly realTimePlayback renderer renderDisplacements renderEffects renderHeight renderPixelAspect renderWidth rendOutputFilename rendSimplifyAreaLights rootNode rootScene sceneMaterials scriptsPath showEndResult skipRenderedFrames sliderTime stackLimit subObjectLevel ticksPerFrame timeDisplayMode trackViewNodes useEnvironmentMap videoPostTracks";
+            string[] strs = s.Split(' ');
+            strs.ForEach(x => Debug.WriteLine($"<Word>{x}</Word>"));
+            //List<string> strings = new List<string>();
+            //foreach(string s in MConstants.ParserColors["DarkGreenText"])
+            //{
+            //    int dotIndex = s.IndexOf(".");
+            //    if (dotIndex != -1)
+            //    {
+            //        string title = s.Substring(0, dotIndex);
+            //        if (!strings.Contains(title))
+            //            strings.Add(title);
+            //    }
+                
+            //}
+
+            //strings.ForEach(x => Debug.WriteLine(x));
         }
 
 
